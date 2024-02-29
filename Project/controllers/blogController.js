34f41a974d0aa77f6fc2a506b63dd460b89bc8eb
RodrigoSFS <Rodrigo29339@gmail.com>
2024-02-29 17:11:18 -0300

    Creation of the controllers folder completing the MVC on the project nad separation of the blogs views

---
+++ Project/controllers/blogController.js
@@ -0,0 +1,68 @@
+const Blog = require('../models/blog');
+// The functions will be written on the mdn naming convention.
+// blog_index, blog_details, blog_create_get,  blog_create_post, blog_delete
+
+const blog_index = (req, res) =>{
+    Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+        res.render('blogs/index', { title: 'All Blogs', blogs: result });
+        // console.log(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+};
+
+const blog_details = (req, res) => {
+    const id = req.params.id;
+
+    Blog.findById(id)
+        .then((result) => {
+            res.render('blogs/details', { title: 'Blog details', blog: result});
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+const blog_create_get = (req, res) => {
+    res.render('blogs/create', { title: 'Create'});
+}
+
+const blog_create_post = (req, res) => {
+    // In order to get the data which is submitted no the form we need some middleware. We'll use the one which comes with express.
+    // console.log(req.body);
+
+    const blog = new Blog(req.body); // The body of the POST request is a JSON with the key value pairs that are specified as the atributes for our blog object!
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+            // console.log(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+const blog_delete = (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            // this is a ajax request, so we cant' redirect. We have to send back a JSON or text data back to the browser.
+            // So what that next line does is send a JSON with a redirect property. Look into details.ejs for further explanation of what hrouterens next.
+            res.json({ redirect: '/blogs' });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+module.exports = {
+    blog_index,
+    blog_details,
+    blog_create_get,
+    blog_create_post,
+    blog_delete
+}
--- Project/routes/blogRoutes.js
+++ Project/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController')
 
 const router = express.Router();
 
@@ -17,64 +17,16 @@ const router = express.Router();
 
 // Since we're separating the routers, the first argument of it don't need to bee put on both files, if it is, it'll stack on each other, like /blogs/blogs. so we don't need 
 // to put it in here anymore, just a simple "/", the blogs will be put on the app.use('/blogs', blogRoutes);
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-        res.render('index', { title: 'All Blogs', blogs: result });
-        // console.log(result);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-})
+router.get('/', blogController.blog_index)
 
 // POST handler
-router.post('/', (req, res) => {
-    // In order to get the data which is submitted no the form we need some middleware. We'll use the one which comes with express.
-    // console.log(req.body);
+router.post('/', blogController.blog_create_post)
 
-    const blog = new Blog(req.body); // The body of the POST request is a JSON with the key value pairs that are specified as the atributes for our blog object!
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-            // console.log(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-})
-
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create'});
-});
+router.get('/create', blogController.blog_create_get);
 
 // Route parameters. On 
-router.get('/:id', (req,res) => {
-    const id = req.params.id;
+router.get('/:id', blogController.blog_details);
 
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { title: 'Blog details', blog: result});
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
-});
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-            // this is a ajax request, so we cant' redirect. We have to send back a JSON or text data back to the browser.
-            // So what that next line does is send a JSON with a redirect property. Look into details.ejs for further explanation of what hrouterens next.
-            res.json({ redirect: '/blogs' });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+router.delete('/:id', blogController.blog_delete);
 
 module.exports = router;
