62d6174a22ce305374447cc125a532331572eb37
RodrigoSFS <rodrigo29339@gmail.com>
2024-03-22 17:15:53 -0300

    importing 'path'

--- Project/app.js
+++ Project/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 // const Blog = require('./models/blog');
 const blogRoutes = require('./routes/blogRoutes')
 
